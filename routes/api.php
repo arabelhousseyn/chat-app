57a2d5de5440f3c5518a61f3e23da8edc9402358
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-04-09 22:02:42 +0000

    add auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\{LoginController,RegisterController};
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+
+
+
+
 
 Route::middleware(['throttle:login'])->group(function (){
     Route::post('login',LoginController::class);
@@ -24,3 +17,10 @@ Route::middleware(['throttle:login'])->group(function (){
 
 
 Route::post('register',RegisterController::class);
+
+
+Route::middleware('auth:sanctum')->group(function (){
+
+
+
+});
