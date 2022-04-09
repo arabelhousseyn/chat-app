fa4ae0b12a71dbc7f7c0d0d9a49192df36897c74
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-04-09 22:18:55 +0000

    design the login page

--- public/js/app.js
+++ public/js/app.js
@@ -5357,6 +5357,66 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/LoginPage.vue?vue&type=script&lang=js&":
+/*!***********************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/LoginPage.vue?vue&type=script&lang=js& ***!
+  \***********************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  data: function data() {
+    return {
+      show1: false,
+      data: {
+        phone: null,
+        password: null
+      }
+    };
+  },
+  methods: {
+    login: function login() {}
+  }
+});
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -34633,15 +34693,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _LoginPage_vue_vue_type_template_id_373c9342___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./LoginPage.vue?vue&type=template&id=373c9342& */ "./resources/js/pages/LoginPage.vue?vue&type=template&id=373c9342&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _LoginPage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./LoginPage.vue?vue&type=script&lang=js& */ "./resources/js/pages/LoginPage.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 ;
-var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _LoginPage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _LoginPage_vue_vue_type_template_id_373c9342___WEBPACK_IMPORTED_MODULE_0__.render,
   _LoginPage_vue_vue_type_template_id_373c9342___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
   false,
@@ -34729,6 +34791,22 @@ var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__
 if (false) { var api; }
 component.options.__file = "resources/js/pages/RegisterPage.vue"
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/pages/LoginPage.vue?vue&type=script&lang=js&":
+/*!*******************************************************************!*\
+  !*** ./resources/js/pages/LoginPage.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LoginPage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./LoginPage.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/LoginPage.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LoginPage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
@@ -34844,7 +34922,69 @@ var render = function () {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "login-page" }, [
-    _vm._v("\n    welcome to login page\n"),
+    _c(
+      "div",
+      {
+        staticClass: "wraping d-flex justify-content-center",
+        staticStyle: { "margin-top": "200px" },
+      },
+      [
+        _c(
+          "v-card",
+          { attrs: { width: "500", color: "secondary", dark: "" } },
+          [
+            _c("v-card-title", [_vm._v("Login")]),
+            _vm._v(" "),
+            _c("v-card-text", [
+              _c(
+                "form",
+                {
+                  on: {
+                    submit: function ($event) {
+                      $event.preventDefault()
+                      return _vm.login.apply(null, arguments)
+                    },
+                  },
+                },
+                [
+                  _c("v-text-field", {
+                    attrs: {
+                      label: "Phone",
+                      placeholder: "Phone",
+                      filled: "",
+                      dense: "",
+                    },
+                  }),
+                  _vm._v(" "),
+                  _c("v-text-field", {
+                    attrs: {
+                      "append-icon": _vm.show1 ? "mdi-eye" : "mdi-eye-off",
+                      label: "Password",
+                      placeholder: "Password",
+                      type: _vm.show1 ? "text" : "password",
+                      filled: "",
+                      dense: "",
+                    },
+                    on: {
+                      "click:append": function ($event) {
+                        _vm.show1 = !_vm.show1
+                      },
+                    },
+                  }),
+                  _vm._v(" "),
+                  _c("v-btn", { attrs: { type: "submit", color: "green" } }, [
+                    _vm._v("Log In"),
+                  ]),
+                ],
+                1
+              ),
+            ]),
+          ],
+          1
+        ),
+      ],
+      1
+    ),
   ])
 }
 var staticRenderFns = []
--- resources/js/pages/LoginPage.vue
+++ resources/js/pages/LoginPage.vue
@@ -1,5 +1,49 @@
 <template>
     <div class="login-page">
-        welcome to login page
+        <div class="wraping d-flex justify-content-center" style="margin-top:200px;">
+            <v-card width="500" color="secondary" dark>
+                <v-card-title>Login</v-card-title>
+
+                <v-card-text>
+                    <form @submit.prevent="login">
+                        <v-text-field
+                            label="Phone"
+                            placeholder="Phone"
+                            filled
+                            dense
+                        ></v-text-field>
+                        <v-text-field
+                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                            label="Password"
+                            placeholder="Password"
+                            :type="show1 ? 'text' : 'password'"
+                            filled
+                            dense
+                            @click:append="show1 = !show1"
+                        ></v-text-field>
+
+                        <v-btn type="submit" color="green">Log In</v-btn>
+                    </form>
+                </v-card-text>
+            </v-card>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    data : ()=>({
+        show1: false,
+        data : {
+            phone : null,
+            password : null,
+        }
+    }),
+    methods : {
+        login()
+        {
+
+        }
+    }
+}
+</script>
